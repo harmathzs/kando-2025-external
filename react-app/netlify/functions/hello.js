610c002a88184bfe0e1fd7cb872e5b45f8d29fe4
Zsolt Harmath <harmathzs@gmail.com>
2025-11-17 14:16:48 +0100

    Respond to GET /hello

--- react-app/netlify/functions/hello.js
+++ react-app/netlify/functions/hello.js
@@ -1,4 +1,10 @@
 /** hello.js - GET /hello */
 exports.handler = (event) => {
     console.log('/hello event', event)
+
+    return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify('Hello Lajos')
+    }
 }
